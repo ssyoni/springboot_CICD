23fd46e90879012099dff124e3df6d8230b5d212
ssyoni <jsy950601@gmail.com>
2022-03-23 00:53:43 +0900

    create profile api

--- src/main/java/com/test/miniproject/test/TestController.java
+++ src/main/java/com/test/miniproject/test/TestController.java
@@ -1,16 +1,30 @@
 package com.test.miniproject.test;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
 
-    @GetMapping("/test/")
+    private final Environment env;
+
+    @GetMapping("/")
     public String test(){
-        return "hello world~!!!";
+        List<String> profile = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1","real2");
+        String defaultProfile = profile.isEmpty() ? "default" : profile.get(0);
+
+        return profile.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 }
