dde7873fb3e90608da744930711a99805a12f8c1
ssyoni <jsy950601@gmail.com>
2022-04-12 01:11:18 +0900

    TEST

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -3,13 +3,13 @@
 # 쉬고 있는 profile 찾기: real1이 사용 중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 function find_idle_profile()
 {
-    RESPONSE_CODE=$(sudo curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)  # nginx가 바라보고 있는 스프링 부트가 정상적으로 수행중인지 확인
+    RESPONSE_CODE=$(sudo curl -s -o /dev/null -w "%{http_code}" http://3.35.50.206/profile)  # nginx가 바라보고 있는 스프링 부트가 정상적으로 수행중인지 확인
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함) / 정상 : 200, 오류 400~503
     then
         CURRENT_PROFILE=real2
     else
-        CURRENT_PROFILE=$(sudo curl -s http://localhost/profile)
+        CURRENT_PROFILE=$(sudo curl -s http://3.35.50.206/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == real1 ]
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,7 +9,7 @@ function switch_proxy() {
 
     echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
-    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+    echo "set \$service_url http://3.35.50.206:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
     echo "> 엔진엑스 Reload"
     sudo docker exec -d nginx nginx -s -reload
