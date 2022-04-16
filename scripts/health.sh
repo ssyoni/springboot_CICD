47d5fae6dee5af7c03c17f8dd71ff397c006af05
ssyoni <jsy950601@gmail.com>
2022-04-16 23:32:35 +0900

    TEST

--- scripts/health.sh
+++ scripts/health.sh
@@ -15,7 +15,7 @@ sleep 10
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://3.35.50.206:${IDLE_PORT}/profile)
-  echo ">>>>>> RESPONSE : ${RESPONSE}"
+  echo ">>>>>> RESPONSE : $RESPONSE"
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
