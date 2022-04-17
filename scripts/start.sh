df3b3bfaba3136b1a15d7145461c809a554d09f8
ssyoni <jsy950601@gmail.com>
2022-04-17 15:00:13 +0900

    TEST

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,41 +13,41 @@ echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> yes |cp -arpf $REPOSITORY/zip/dockerfile $REPOSITORY/"
-
-yes |cp -arpf $REPOSITORY/zip/dockerfile $REPOSITORY/
-
-echo "> yes |cp -arpf $REPOSITORY/zip/docker-compose.yml $REPOSITORY/"
-
-yes |cp -arpf $REPOSITORY/zip/docker-compose.yml $REPOSITORY/
-
-echo "> docker-compose down"
-
-docker-compose down
-
-echo "> docker-compose up -d"
-
-docker-compose up -d
-
-#echo "> 새 어플리케이션 배포"
-#JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
-#echo "> JAR Name: $JAR_NAME"
+#echo "> yes |cp -arpf $REPOSITORY/zip/dockerfile $REPOSITORY/"
 #
-#echo "> $JAR_NAME 에 실행권한 추가"
+#yes |cp -arpf $REPOSITORY/zip/dockerfile $REPOSITORY/
+
+#echo "> yes |cp -arpf $REPOSITORY/zip/docker-compose.yml $REPOSITORY/"
 #
-#chmod +x $JAR_NAME
+#yes |cp -arpf $REPOSITORY/zip/docker-compose.yml $REPOSITORY/
+
+#echo "> docker-compose down"
 #
-#echo "> $JAR_NAME 실행"
+#docker-compose down
 #
-#IDLE_PROFILE=$(find_idle_profile)
+#echo "> docker-compose up -d"
 #
-#echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-#
-#cd $REPOSITORY
-#docker build -t spring ./
-#
-#
-#echo "> docker run -it --name $IDLE_PROFILE -d -e active=$IDLE_PROFILE -p $IDLE_PORT:$IDLE_PORT spring"
-#
-#docker run -it --name "$IDLE_PROFILE" -d -e active=$IDLE_PROFILE -p $IDLE_PORT:$IDLE_PORT spring
+#docker-compose up -d
+
+echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+
+cd $REPOSITORY
+docker build -t spring ./
+
+
+echo "> docker run -it --name $IDLE_PROFILE -d -e active=$IDLE_PROFILE -p $IDLE_PORT:$IDLE_PORT spring"
+
+docker run -it --name "$IDLE_PROFILE" -d -e active=$IDLE_PROFILE -p $IDLE_PORT:$IDLE_PORT spring
